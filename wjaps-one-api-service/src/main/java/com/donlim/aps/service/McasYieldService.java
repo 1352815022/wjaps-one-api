147127a6b249bb991053f5ae0bf20330f5c647bf
LiJJ <441390451@qq.com>
2023-04-12 11:02:14 +0800

    dev-20230412:编程式事务写法修改

--- wjaps-one-api-service/src/main/java/com/donlim/aps/service/McasYieldService.java
+++ wjaps-one-api-service/src/main/java/com/donlim/aps/service/McasYieldService.java
@@ -3,15 +3,21 @@ package com.donlim.aps.service;
 import com.changhong.sei.core.dao.BaseEntityDao;
 import com.changhong.sei.core.dto.serach.Search;
 import com.changhong.sei.core.dto.serach.SearchFilter;
+import com.changhong.sei.core.log.LogUtil;
 import com.changhong.sei.core.service.BaseEntityService;
+import com.changhong.sei.core.utils.TransactionUtil;
 import com.donlim.aps.connector.McasConnector;
 import com.donlim.aps.dao.McasYieldDao;
 import com.donlim.aps.entity.McasYield;
 import com.donlim.aps.util.CompanyEnum;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -32,6 +38,10 @@ public class McasYieldService extends BaseEntityService<McasYield> {
     private ApsProductionProcessScheduleService apsProductionProcessScheduleService;
     @Autowired
     private ApsOrderPlanService apsOrderPlanService;
+
+
+
+
     @Override
     protected BaseEntityDao<McasYield> getDao() {
         return dao;
@@ -63,6 +73,48 @@ public class McasYieldService extends BaseEntityService<McasYield> {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+    }
+
+
+    /**
+     * 更新产量信息并更新产量报表2.0
+     */
+    public void updateYeildTask_V2(){
+
+        //更新时间为当天8点前，获取昨天的
+        LocalDate date=LocalDate.now();
+        if(LocalDateTime.now().getHour() < 8){
+            date=LocalDate.now().plusDays(-1);
+        }
+        List<McasYield> yieldData = null;
+        List<McasYield> manualYieldData = null;
+        try {
+            yieldData = McasConnector.getCYYieldData(CompanyEnum.WJ1_MCAS.getCode(), date);
+            manualYieldData = McasConnector.getManualYieldData(CompanyEnum.WJ1_MCAS.getCode(), date);
+            yieldData.addAll(manualYieldData);
+        } catch (Exception e) {
+            e.printStackTrace();
+            LogUtil.bizLog("数据获取失败了，{}",e.getMessage());
+        }
+        //开启编程式事务
+        TransactionStatus saveStatus = TransactionUtil.beginNewTransaction();
+        try{
+            dao.deleteByDateEquals(date);
+            dao.save(yieldData);
+            //重新计算进度表
+            apsProductionProcessScheduleService.calcProductionProcessSchedule(date);
+            if(LocalDateTime.now().getHour() == 7) {
+                apsOrderPlanService.updatePlanFromMcas();
+            }
+            TransactionUtil.commit(saveStatus);
+        }catch (Exception e){
+            TransactionUtil.rollback(saveStatus);
+            LogUtil.bizLog("updateYeildTask_V2事务回滚了，{}",e.getMessage());
+        }
+
+
+
     }
 
     /**
